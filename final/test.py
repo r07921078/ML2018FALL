5a49c2ed3c74e2c42aad71812e6773b59ef0d3ee
Ting Wei Zhang <r07921078@ntu.edu.tw>
2018-12-08 21:09:24 +0800

    v1, Kaggle:0.296

---
+++ final/test.py
@@ -0,0 +1,112 @@
+import os, sys, math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+from imgaug import augmenters as iaa
+from tqdm import tqdm
+
+import warnings
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, load_model
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Input
+from keras.layers import BatchNormalization
+from keras.layers import Conv2D
+from keras.models import Model
+from keras.applications import InceptionResNetV2
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import LambdaCallback
+from keras.callbacks import Callback
+from keras import metrics
+from keras.optimizers import Adam 
+from keras import backend as K
+import tensorflow as tf
+import keras
+
+def f1(y_true, y_pred):
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
+
+
+class data_generator:
+    
+    def create_train(dataset_info, batch_size, shape, augument=True):
+        assert shape[2] == 3
+        while True:
+            random_indexes = np.random.choice(len(dataset_info), batch_size)
+            batch_images = np.empty((batch_size, shape[0], shape[1], shape[2]))
+            batch_labels = np.zeros((batch_size, 28))
+            for i, idx in enumerate(random_indexes):
+                image = data_generator.load_image(
+                    dataset_info[idx]['path'], shape)   
+                if augument:
+                    image = data_generator.augment(image)
+                batch_images[i] = image
+                batch_labels[i][dataset_info[idx]['labels']] = 1
+            yield batch_images, batch_labels
+            
+    
+    def load_image(path, shape):
+        R = np.array(Image.open(path+'_red.png'))
+        G = np.array(Image.open(path+'_green.png'))
+        B = np.array(Image.open(path+'_blue.png'))
+        Y = np.array(Image.open(path+'_yellow.png'))
+
+        image = np.stack((
+            R/2 + Y/2, 
+            G/2 + Y/2, 
+            B),-1)
+        
+        image = cv2.resize(image, (shape[0], shape[1]))
+        image = np.divide(image, 255)
+        return image  
+                
+            
+    def augment(image):
+        augment_img = iaa.Sequential([
+            iaa.OneOf([
+                iaa.Affine(rotate=0),
+                iaa.Affine(rotate=90),
+                iaa.Affine(rotate=180),
+                iaa.Affine(rotate=270),
+                iaa.Fliplr(0.5),
+                iaa.Flipud(0.5),
+            ])], random_order=True)
+        
+        image_aug = augment_img.augment_image(image)
+        return image_aug
+    
+    
+INPUT_SHAPE = (299,299,3)
+
+model = load_model('model1.h5', custom_objects={'f1': f1})
+
+submit = pd.read_csv('sample_submission.csv')
+
+
+predicted = []
+for name in tqdm(submit['Id']):
+    path = os.path.join('test/', name)
+    image = data_generator.load_image(path, INPUT_SHAPE)
+    score_predict = model.predict(image[np.newaxis])[0]
+    label_predict = np.arange(28)[score_predict>=0.2]
+    str_predict_label = ' '.join(str(l) for l in label_predict)
+    predicted.append(str_predict_label)
+    
+submit['Predicted'] = predicted
+submit.to_csv('submission.csv', index=False)
+
