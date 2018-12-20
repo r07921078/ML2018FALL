9a127e7131cb68c71d7384de4fc91a9e0297f52e
Ting Wei Zhang <r07921078@ntu.edu.tw>
2018-12-21 01:10:31 +0800

    modify script for split model

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cat word2vec.model.* > word2vec.model
 python3 test.py $1 $2 $3
--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cat word2vec.model.* > word2vec.model
 python3 word2vec_mod.py $1 $2 $3 $4
 python3 train.py $1 $2 $3 $4
